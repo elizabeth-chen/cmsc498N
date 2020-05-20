e0dc9853fc1be957933950238a27539ed98b1a23
hannahjrosen <hrose101@gmail.com>
2020-05-19 21:25:42 -0400

    debug

--- public/sketch.js
+++ public/sketch.js
@@ -142,8 +142,8 @@ function setup() {
   markTypes.push(mark4);
   markTypes.push(mark5);
 
-  socket = io.connect('http://cleft.fun:30000');
-  //socket = io.connect('http://localhost:3000');
+  //socket = io.connect('http://cleft.fun:30000');
+  socket = io.connect('http://localhost:30000');
 
   openSimplex = new OpenSimplexNoise2D(Date.now());
 
@@ -463,8 +463,8 @@ function draw() {
 
     //current cart's data to send to the server
     var cartData = {
-      x: x,
-      y: y,
+      x: x+worldOffset.x,
+      y: yx+worldOffset.y,
       dir: dir,
       items: numItems,
     };
